eaf98340b24eb66ae6a00224f78809ad88f1c3ff
George Hotz <geohot@gmail.com>
2018-05-27 15:03:24 -0700

    normalized coords

--- extractor.py
+++ extractor.py
@@ -1,14 +1,31 @@
 import cv2
 import numpy as np
+np.set_printoptions(suppress=True)
 
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
+from skimage.transform import EssentialMatrixTransform
+
+# turn [[x,y]] -> [[x,y,1]]
+def add_ones(x):
+  return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
 
 class Extractor(object):
-  def __init__(self):
+  def __init__(self, K):
     self.orb = cv2.ORB_create()
     self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
     self.last = None
+    self.K = K
+    self.Kinv = np.linalg.inv(self.K)
+
+  def normalize(self, pts):
+    return np.dot(self.Kinv, add_ones(pts).T).T[:, 0:2]
+
+  def denormalize(self, pt):
+    ret = np.dot(self.K, np.array([pt[0], pt[1], 1.0]))
+    print(ret)
+    #ret /= ret[2]
+    return int(round(ret[0])), int(round(ret[1]))
 
   def extract(self, img):
     # detection
@@ -31,12 +48,21 @@ class Extractor(object):
     # filter
     if len(ret) > 0:
       ret = np.array(ret)
+
+      # normalize coords
+      ret[:, 0, :] = self.normalize(ret[:, 0, :])
+      ret[:, 1, :] = self.normalize(ret[:, 1, :])
+
       model, inliers = ransac((ret[:, 0], ret[:, 1]),
+                              #EssentialMatrixTransform,
                               FundamentalMatrixTransform,
                               min_samples=8,
                               residual_threshold=1,
                               max_trials=100)
       ret = ret[inliers]
+
+      #s,v,d = np.linalg.svd(model.params)
+      #print(v)
 
     # return
     self.last = {'kps': kps, 'des': des}
--- slam.py
+++ slam.py
@@ -8,8 +8,11 @@ import numpy as np
 W = 1920//2
 H = 1080//2
 
+F = 1
 disp = Display(W, H)
-fe = Extractor()
+K = np.array([[F,0,W//2],[0,F,H//2],[0,0,1]])
+print(K)
+fe = Extractor(K)
 
 def process_frame(img):
   img = cv2.resize(img, (W,H))
@@ -18,8 +21,9 @@ def process_frame(img):
   print("%d matches" % (len(matches)))
 
   for pt1, pt2 in matches:
-    u1,v1 = map(lambda x: int(round(x)), pt1)
-    u2,v2 = map(lambda x: int(round(x)), pt2)
+    u1,v1 = fe.denormalize(pt1)
+    u2,v2 = fe.denormalize(pt2)
+
     cv2.circle(img, (u1, v1), color=(0,255,0), radius=3)
     cv2.line(img, (u1, v1), (u2, v2), color=(255,0,0))
 
