e9bd70371700e2c3b3ad33736802ce5a4ccfce15
George Hotz <geohot@gmail.com>
2018-05-28 12:27:20 -0700

    be 20 bro, not 10 bro

--- pointmap.py
+++ pointmap.py
@@ -107,7 +107,7 @@ class Map(object):
     if verbose:
       opt.set_verbose(True)
     opt.initialize_optimization()
-    opt.optimize(10)
+    opt.optimize(20)
 
     # put frames back
     for f in self.frames:
