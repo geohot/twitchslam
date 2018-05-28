048eaa9bd81905e16287e64f7a9ed0dae3c5b365
George Hotz <geohot@gmail.com>
2018-05-27 18:58:04 -0700

    less stupid triangulate function:

--- slam.py
+++ slam.py
@@ -19,7 +19,18 @@ mapp = Map()
 disp = Display(W, H) if os.getenv("D2D") is not None else None
 
 def triangulate(pose1, pose2, pts1, pts2):
-  return cv2.triangulatePoints(pose1[:3], pose2[:3], pts1.T, pts2.T).T
+  ret = np.zeros((pts1.shape[0], 4))
+  pose1 = np.linalg.inv(pose1)
+  pose2 = np.linalg.inv(pose2)
+  for i, p in enumerate(zip(pts1, pts2)):
+    A = np.zeros((4,4))
+    A[0] = p[0][0] * pose1[2] - pose1[0]
+    A[1] = p[0][1] * pose1[2] - pose1[1]
+    A[2] = p[1][0] * pose2[2] - pose2[0]
+    A[3] = p[1][1] * pose2[2] - pose2[1]
+    _, _, vt = np.linalg.svd(A)
+    ret[i] = vt[3]
+  return ret
 
 def process_frame(img):
   img = cv2.resize(img, (W,H))
@@ -40,6 +51,7 @@ def process_frame(img):
   # reject pts without enough "parallax" (this right?)
   # reject points behind the camera
   good_pts4d = (np.abs(pts4d[:, 3]) > 0.005) & (pts4d[:, 2] > 0)
+  #print(sum(good_pts4d), len(good_pts4d))
 
   for i,p in enumerate(pts4d):
     if not good_pts4d[i]:
