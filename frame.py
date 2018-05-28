bc592dc3c9ca5d1744fcfdf19c8b010ca3937e0a
George Hotz <geohot@gmail.com>
2018-05-27 17:30:30 -0700

    refactor to keep indices

--- frame.py
+++ frame.py
@@ -10,6 +10,9 @@ from skimage.transform import EssentialMatrixTransform
 def add_ones(x):
   return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
 
+IRt = np.eye(4)
+
+# pose
 def extractRt(E):
   W = np.mat([[0,-1,0],[1,0,0],[0,0,1]],dtype=float)
   U,d,Vt = np.linalg.svd(E)
@@ -20,8 +23,10 @@ def extractRt(E):
   if np.sum(R.diagonal()) < 0:
     R = np.dot(np.dot(U, W.T), Vt)
   t = U[:, 2]
-  Rt = np.concatenate([R,t.reshape(3,1)], axis=1)
-  return Rt
+  ret = np.eye(4)
+  ret[:3, :3] = R
+  ret[:3, 3] = t
+  return ret
 
 def extract(img):
   orb = cv2.ORB_create()
@@ -43,19 +48,27 @@ def denormalize(K, pt):
   ret /= ret[2]
   return int(round(ret[0])), int(round(ret[1]))
 
-def match(f1, f2):
+def match_frames(f1, f2):
   bf = cv2.BFMatcher(cv2.NORM_HAMMING)
   matches = bf.knnMatch(f1.des, f2.des, k=2)
 
   # Lowe's ratio test
   ret = []
+  idx1,idx2 = [], []
+
   for m,n in matches:
     if m.distance < 0.75*n.distance:
+      # keep around indices
+      idx1.append(m.queryIdx)
+      idx2.append(m.trainIdx)
+
       p1 = f1.pts[m.queryIdx]
       p2 = f2.pts[m.trainIdx]
       ret.append((p1, p2))
   assert len(ret) >= 8
   ret = np.array(ret)
+  idx1 = np.array(idx1)
+  idx2 = np.array(idx2)
 
   # fit matrix
   model, inliers = ransac((ret[:, 0], ret[:, 1]),
@@ -68,16 +81,16 @@ def match(f1, f2):
   print(sum(inliers), len(inliers))
 
   # ignore outliers
-  ret = ret[inliers]
   Rt = extractRt(model.params)
 
   # return
-  return ret, Rt
+  return idx1[inliers], idx2[inliers], Rt
 
 class Frame(object):
   def __init__(self, img, K):
     self.K = K
     self.Kinv = np.linalg.inv(self.K)
+    self.pose = IRt
 
     pts, self.des = extract(img)
     self.pts = normalize(self.Kinv, pts)
--- slam.py
+++ slam.py
@@ -2,7 +2,7 @@
 import time
 import cv2
 from display import Display
-from frame import Frame, denormalize, match
+from frame import Frame, denormalize, match_frames, IRt
 import numpy as np
 import g2o
 
@@ -15,6 +15,22 @@ K = np.array([[F,0,W//2],[0,F,H//2],[0,0,1]])
 # main classes
 disp = Display(W, H)
 
+class Point(object):
+  # A Point is a 3-D point in the world
+  # Each Point is observed in multiple Frames
+
+  def __init__(self, loc):
+    self.location = loc
+    self.frames = []
+    self.idxs = []
+
+  def add_observation(self, frame, idx):
+    self.frames.append(frame)
+    self.idxs.append(idx)
+
+def triangulate(pose1, pose2, pts1, pts2):
+  return cv2.triangulatePoints(pose1[:3], pose2[:3], pts1.T, pts2.T).T
+
 frames = []
 def process_frame(img):
   img = cv2.resize(img, (W,H))
@@ -23,9 +39,25 @@ def process_frame(img):
   if len(frames) <= 1:
     return
 
-  ret, Rt = match(frames[-1], frames[-2])
+  idx1, idx2, Rt = match_frames(frames[-1], frames[-2])
+  frames[-1].pose = np.dot(Rt, frames[-2].pose)
 
-  for pt1, pt2 in ret:
+  # homogeneous 3-D coords
+  pts4d = triangulate(frames[-1].pose, frames[-2].pose, frames[-1].pts[idx1], frames[-2].pts[idx2])
+  pts4d /= pts4d[:, 3:]
+
+  # reject pts without enough "parallax" (this right?)
+  # reject points behind the camera
+  good_pts4d = (np.abs(pts4d[:, 3]) > 0.005) & (pts4d[:, 2] > 0)
+
+  for i,p in enumerate(pts4d):
+    if not good_pts4d[i]:
+      continue
+    pt = Point(p)
+    pt.add_observation(frames[-1], idx1[i])
+    pt.add_observation(frames[-2], idx2[i])
+
+  for pt1, pt2 in zip(frames[-1].pts[idx1], frames[-2].pts[idx2]):
     u1, v1 = denormalize(K, pt1)
     u2, v2 = denormalize(K, pt2)
     cv2.circle(img, (u1, v1), color=(0,255,0), radius=3)
