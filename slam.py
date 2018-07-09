fa70cd69d1c54a593cb7a44ada852fa03092cd44
George Hotz <geohot@gmail.com>
2018-07-08 23:59:29 -0700

    refactor into class

--- slam.py
+++ slam.py
@@ -16,180 +16,184 @@ from helpers import triangulate, add_ones, myjet
 
 np.set_printoptions(suppress=True)
 
-# main classes
-mapp = Map()
-disp2d = None
-disp3d = None
+class SLAM(object):
+  def __init__(self, W, H, K, disp2d=None):
+    # main classes
+    self.mapp = Map()
+    self.disp2d = disp2d
 
-def process_frame(img, pose=None):
-  start_time = time.time()
-  img = cv2.resize(img, (W,H))
-  frame = Frame(mapp, img, K)
-  if frame.id == 0:
-    return
+    # params
+    self.W, self.H = W, H
+    self.K = K
 
-  f1 = mapp.frames[-1]
-  f2 = mapp.frames[-2]
+  def process_frame(self, img, pose=None):
+    start_time = time.time()
+    img = cv2.resize(img, (self.W, self.H))
+    frame = Frame(self.mapp, img, self.K)
+    if frame.id == 0:
+      return
 
-  idx1, idx2, Rt = match_frames(f1, f2)
+    f1 = self.mapp.frames[-1]
+    f2 = self.mapp.frames[-2]
 
-  # add new observations if the point is already observed in the previous frame
-  # TODO: consider tradeoff doing this before/after search by projection
-  for i,idx in enumerate(idx2):
-    if f2.pts[idx] is not None and f1.pts[idx1[i]] is None:
-      f2.pts[idx].add_observation(f1, idx1[i])
+    idx1, idx2, Rt = match_frames(f1, f2)
 
-  if frame.id < 5 or True:
-    # get initial positions from fundamental matrix
-    f1.pose = np.dot(Rt, f2.pose)
-  else:
-    # kinematic model (not used)
-    velocity = np.dot(f2.pose, np.linalg.inv(mapp.frames[-3].pose))
-    f1.pose = np.dot(velocity, f2.pose)
+    # add new observations if the point is already observed in the previous frame
+    # TODO: consider tradeoff doing this before/after search by projection
+    for i,idx in enumerate(idx2):
+      if f2.pts[idx] is not None and f1.pts[idx1[i]] is None:
+        f2.pts[idx].add_observation(f1, idx1[i])
 
-  # pose optimization
-  if pose is None:
-    #print(f1.pose)
-    pose_opt = mapp.optimize(local_window=1, fix_points=True)
-    print("Pose:     %f" % pose_opt)
-    #print(f1.pose)
-  else:
-    # have ground truth for pose
-    f1.pose = pose
+    if frame.id < 5 or True:
+      # get initial positions from fundamental matrix
+      f1.pose = np.dot(Rt, f2.pose)
+    else:
+      # kinematic model (not used)
+      velocity = np.dot(f2.pose, np.linalg.inv(self.mapp.frames[-3].pose))
+      f1.pose = np.dot(velocity, f2.pose)
 
-  sbp_pts_count = 0
+    # pose optimization
+    if pose is None:
+      #print(f1.pose)
+      pose_opt = self.mapp.optimize(local_window=1, fix_points=True)
+      print("Pose:     %f" % pose_opt)
+      #print(f1.pose)
+    else:
+      # have ground truth for pose
+      f1.pose = pose
 
-  # search by projection
-  if len(mapp.points) > 0:
-    # project *all* the map points into the current frame
-    map_points = np.array([p.homogeneous() for p in mapp.points])
-    projs = np.dot(np.dot(K, f1.pose[:3]), map_points.T).T
-    projs = projs[:, 0:2] / projs[:, 2:]
+    sbp_pts_count = 0
 
-    # only the points that fit in the frame
-    good_pts = (projs[:, 0] > 0) & (projs[:, 0] < W) & \
-               (projs[:, 1] > 0) & (projs[:, 1] < H)
+    # search by projection
+    if len(self.mapp.points) > 0:
+      # project *all* the map points into the current frame
+      map_points = np.array([p.homogeneous() for p in self.mapp.points])
+      projs = np.dot(np.dot(K, f1.pose[:3]), map_points.T).T
+      projs = projs[:, 0:2] / projs[:, 2:]
 
-    for i, p in enumerate(mapp.points):
-      if not good_pts[i]:
-        # point not visible in frame
+      # only the points that fit in the frame
+      good_pts = (projs[:, 0] > 0) & (projs[:, 0] < self.W) & \
+                 (projs[:, 1] > 0) & (projs[:, 1] < self.H)
+
+      for i, p in enumerate(self.mapp.points):
+        if not good_pts[i]:
+          # point not visible in frame
+          continue
+        if f1 in p.frames:
+          # we already matched this map point to this frame
+          # TODO: understand this better
+          continue
+        for m_idx in f1.kd.query_ball_point(projs[i], 2):
+          # if point unmatched
+          if f1.pts[m_idx] is None:
+            b_dist = p.orb_distance(f1.des[m_idx])
+            # if any descriptors within 64
+            if b_dist < 64.0:
+              p.add_observation(f1, m_idx)
+              sbp_pts_count += 1
+              break
+
+    # triangulate the points we don't have matches for
+    good_pts4d = np.array([f1.pts[i] is None for i in idx1])
+
+    # do triangulation in global frame
+    pts4d = triangulate(f1.pose, f2.pose, f1.kps[idx1], f2.kps[idx2])
+    good_pts4d &= np.abs(pts4d[:, 3]) != 0
+    pts4d /= pts4d[:, 3:]       # homogeneous 3-D coords
+
+    # adding new points to the map from pairwise matches
+    new_pts_count = 0
+    for i,p in enumerate(pts4d):
+      if not good_pts4d[i]:
         continue
-      if f1 in p.frames:
-        # we already matched this map point to this frame
-        # TODO: understand this better
+
+      # check parallax is large enough
+      # TODO: learn what parallax means
+      """
+      r1 = np.dot(f1.pose[:3, :3], add_ones(f1.kps[idx1[i]]))
+      r2 = np.dot(f2.pose[:3, :3], add_ones(f2.kps[idx2[i]]))
+      parallax = r1.dot(r2) / (np.linalg.norm(r1) * np.linalg.norm(r2))
+      if parallax >= 0.9998:
         continue
-      for m_idx in f1.kd.query_ball_point(projs[i], 2):
-        # if point unmatched
-        if f1.pts[m_idx] is None:
-          b_dist = p.orb_distance(f1.des[m_idx])
-          # if any descriptors within 64
-          if b_dist < 64.0:
-            p.add_observation(f1, m_idx)
-            sbp_pts_count += 1
-            break
+      """
 
-  # triangulate the points we don't have matches for
-  good_pts4d = np.array([f1.pts[i] is None for i in idx1])
+      # check points are in front of both cameras
+      pl1 = np.dot(f1.pose, p)
+      pl2 = np.dot(f2.pose, p)
+      if pl1[2] < 0 or pl2[2] < 0:
+        continue
 
-  # do triangulation in global frame
-  pts4d = triangulate(f1.pose, f2.pose, f1.kps[idx1], f2.kps[idx2])
-  good_pts4d &= np.abs(pts4d[:, 3]) != 0
-  pts4d /= pts4d[:, 3:]       # homogeneous 3-D coords
+      # reproject
+      pp1 = np.dot(K, pl1[:3])
+      pp2 = np.dot(K, pl2[:3])
 
-  # adding new points to the map from pairwise matches
-  new_pts_count = 0
-  for i,p in enumerate(pts4d):
-    if not good_pts4d[i]:
-      continue
+      # check reprojection error
+      pp1 = (pp1[0:2] / pp1[2]) - f1.kpus[idx1[i]]
+      pp2 = (pp2[0:2] / pp2[2]) - f2.kpus[idx2[i]]
+      pp1 = np.sum(pp1**2)
+      pp2 = np.sum(pp2**2)
+      if pp1 > 2 or pp2 > 2:
+        continue
 
-    # check parallax is large enough
-    # TODO: learn what parallax means
-    """
-    r1 = np.dot(f1.pose[:3, :3], add_ones(f1.kps[idx1[i]]))
-    r2 = np.dot(f2.pose[:3, :3], add_ones(f2.kps[idx2[i]]))
-    parallax = r1.dot(r2) / (np.linalg.norm(r1) * np.linalg.norm(r2))
-    if parallax >= 0.9998:
-      continue
-    """
+      # add the point
+      color = img[int(round(f1.kpus[idx1[i],1])), int(round(f1.kpus[idx1[i],0]))]
+      pt = Point(self.mapp, p[0:3], color)
+      pt.add_observation(f2, idx2[i])
+      pt.add_observation(f1, idx1[i])
+      new_pts_count += 1
 
-    # check points are in front of both cameras
-    pl1 = np.dot(f1.pose, p)
-    pl2 = np.dot(f2.pose, p)
-    if pl1[2] < 0 or pl2[2] < 0:
-      continue
+    print("Adding:   %d new points, %d search by projection" % (new_pts_count, sbp_pts_count))
 
-    # reproject
-    pp1 = np.dot(K, pl1[:3])
-    pp2 = np.dot(K, pl2[:3])
+    # optimize the map
+    if frame.id >= 4 and frame.id%5 == 0:
+      err = self.mapp.optimize() #verbose=True)
+      print("Optimize: %f units of error" % err)
 
-    # check reprojection error
-    pp1 = (pp1[0:2] / pp1[2]) - f1.kpus[idx1[i]]
-    pp2 = (pp2[0:2] / pp2[2]) - f2.kpus[idx2[i]]
-    pp1 = np.sum(pp1**2)
-    pp2 = np.sum(pp2**2)
-    if pp1 > 2 or pp2 > 2:
-      continue
+    print("Map:      %d points, %d frames" % (len(self.mapp.points), len(self.mapp.frames)))
+    print("Time:     %.2f ms" % ((time.time()-start_time)*1000.0))
+    print(np.linalg.inv(f1.pose))
 
-    # add the point
-    color = img[int(round(f1.kpus[idx1[i],1])), int(round(f1.kpus[idx1[i],0]))]
-    pt = Point(mapp, p[0:3], color)
-    pt.add_observation(f2, idx2[i])
-    pt.add_observation(f1, idx1[i])
-    new_pts_count += 1
-
-  print("Adding:   %d new points, %d search by projection" % (new_pts_count, sbp_pts_count))
-
-
-  # 2-D display
-  if disp2d is not None:
-    # paint annotations on the image
-    for i1 in idx1:
-      u1, v1 = int(round(f1.kpus[i1][0])), int(round(f1.kpus[i1][1]))
-      if f1.pts[i1] is not None:
-        if len(f1.pts[i1].frames) >= 5:
-          cv2.circle(img, (u1, v1), color=(0,255,0), radius=3)
+    # 2-D display
+    if self.disp2d is not None:
+      # paint annotations on the image
+      for i1 in idx1:
+        u1, v1 = int(round(f1.kpus[i1][0])), int(round(f1.kpus[i1][1]))
+        if f1.pts[i1] is not None:
+          if len(f1.pts[i1].frames) >= 5:
+            cv2.circle(img, (u1, v1), color=(0,255,0), radius=3)
+          else:
+            cv2.circle(img, (u1, v1), color=(0,128,0), radius=3)
+          # draw the trail
+          pts = []
+          lfid = None
+          for f, idx in zip(f1.pts[i1].frames[-9:][::-1], f1.pts[i1].idxs[-9:][::-1]):
+            if lfid is not None and lfid-1 != f.id:
+              break
+            pts.append(tuple(map(lambda x: int(round(x)), f.kpus[idx])))
+            lfid = f.id
+          if len(pts) >= 2:
+            cv2.polylines(img, np.array([pts], dtype=np.int32), False, myjet[len(pts)]*255, thickness=1, lineType=16)
         else:
-          cv2.circle(img, (u1, v1), color=(0,128,0), radius=3)
-        # draw the trail
-        pts = []
-        lfid = None
-        for f, idx in zip(f1.pts[i1].frames[-9:][::-1], f1.pts[i1].idxs[-9:][::-1]):
-          if lfid is not None and lfid-1 != f.id:
-            break
-          pts.append(tuple(map(lambda x: int(round(x)), f.kpus[idx])))
-          lfid = f.id
-        if len(pts) >= 2:
-          cv2.polylines(img, np.array([pts], dtype=np.int32), False, myjet[len(pts)]*255, thickness=1, lineType=16)
-      else:
-        cv2.circle(img, (u1, v1), color=(0,0,0), radius=3)
-    disp2d.paint(img)
+          cv2.circle(img, (u1, v1), color=(0,0,0), radius=3)
+      self.disp2d.paint(img)
 
-  # optimize the map
-  if frame.id >= 4 and frame.id%5 == 0:
-    err = mapp.optimize() #verbose=True)
-    print("Optimize: %f units of error" % err)
-
-  # 3-D display
-  if disp3d is not None:
-    disp3d.paint(mapp)
-
-  print("Map:      %d points, %d frames" % (len(mapp.points), len(mapp.frames)))
-  print("Time:     %.2f ms" % ((time.time()-start_time)*1000.0))
-  print(np.linalg.inv(f1.pose))
 
 if __name__ == "__main__":
   if len(sys.argv) < 2:
     print("%s <video.mp4>" % sys.argv[0])
     exit(-1)
     
-  # create displays and open file
   if os.getenv("HEADLESS") is None:
     disp3d = Display3D()
+  else:
+    disp3d = None
+
   cap = cv2.VideoCapture(sys.argv[1])
 
   # camera parameters
   W = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
   H = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
   CNT = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
   F = float(os.getenv("F", "525"))
   if os.getenv("SEEK") is not None:
@@ -206,8 +210,11 @@ if __name__ == "__main__":
   K = np.array([[F,0,W//2],[0,F,H//2],[0,0,1]])
   Kinv = np.linalg.inv(K)
 
+  # create 2-D display
   if os.getenv("HEADLESS") is None:
     disp2d = Display2D(W, H)
+
+  slam = SLAM(W, H, K, disp2d)
 
   """
   mapp.deserialize(open('map.json').read())
@@ -227,9 +234,14 @@ if __name__ == "__main__":
     ret, frame = cap.read()
     print("\n*** frame %d/%d ***" % (i, CNT))
     if ret == True:
-      process_frame(frame, None if gt_pose is None else np.linalg.inv(gt_pose[i]))
+      slam.process_frame(frame, None if gt_pose is None else np.linalg.inv(gt_pose[i]))
     else:
       break
+
+    # 3-D display
+    if disp3d is not None:
+      disp3d.paint(slam.mapp)
+
     i += 1
     """
     if i == 10:
