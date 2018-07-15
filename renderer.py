15be8061bb9d6975a6204d33a25d5a6053e85a43
motherf <no.0000000000009@gmail.com>
2018-07-15 14:17:11 +0100

    wёrks

--- renderer.py
+++ renderer.py
@@ -35,10 +35,13 @@ class Renderer(object):
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
     glutInitWindowSize(self.W, self.H)
     glutCreateWindow(b"OpenGL Offscreen")
-    glutHideWindow()
+#    glutHideWindow()
+    glutDisplayFunc(self.draw)
+    glutMainLoop()
 
   def draw(self):
-    glClearColor(0.5, 0.5, 0.5, 1.0)
+    glClearColor(1., 0.5, 0.5, 1.0)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glColor(0.0, 1.0, 0.0)
     gluOrtho2D(-1.0, 1.0, -1.0, 1.0)
     glViewport(0, 0, self.W, self.H)
@@ -52,6 +55,7 @@ class Renderer(object):
     glPixelStorei(GL_PACK_ALIGNMENT, 1)
     ret = glReadPixels(0, 0, self.W, self.H, GL_RGBA, GL_UNSIGNED_BYTE)
     ret = np.fromstring(ret, np.uint8).reshape((self.W, self.H, 4))
+    glutSwapBuffers()
     return ret[:, :, 0:3]
 
 if __name__ == "__main__":
@@ -65,8 +69,3 @@ if __name__ == "__main__":
 
   from display import Display2D
   disp2d = Display2D(W, H)
-  while 1:
-    disp2d.paint(draw)
-    time.sleep(1)
-
-
